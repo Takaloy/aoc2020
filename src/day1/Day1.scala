5569d9b06680fc829d42621f627ba05dfbc79600
Takaloy <4484516+Takaloy@users.noreply.github.com>
2020-12-08 11:46:40 +0000

    add comments about day 1 solution being imperfect

--- src/day1/Day1.scala
+++ src/day1/Day1.scala
@@ -1,10 +1,38 @@
 package day1
 import scala.io.Source
 
+/**
+ * This solution is quick and dirty to get to the right answer but it is the wrong solution.
+ * When you pick any two numbers (or three), you might come into a situation where it picks itself.
+ * in this scenario 2020, if any of the input is 1010, then the current solution will present a wrong result,
+ * as the request is to use any TWO numbers not pick itself twice.
+ *
+ * test for wrongness :
+ * part 1
+ *  t = (a*2)
+ * part 2
+ *  t = (a*2) + b
+ *
+ * proposed solution when I have time ...
+ * convert lines to linked list. which looks something like this ...
+ *
+ * def isCorrectCombo(a, b) {
+ *  if (b is Nothing)
+ *    return false
+ *  else if (a + b == 2020)
+ *    return true
+ *  else
+ *    return isCorrectCombo(a, b.tail)
+ * }
+ *
+ * val answer = for {
+ *  number <- numbers if isCorrectCombo(number,number.tail)
+ * } yield number * number.tail
+ */
 object Day1 extends App {
 
   val fileName = "src/day1/input.txt"
-  var lines  = Source.fromFile(fileName).getLines().toArray
+  val lines  = Source.fromFile(fileName).getLines().toArray
 
   //part 1
   val combinations = lines.flatMap(a => lines.map(b => (a.toInt,b.toInt)))

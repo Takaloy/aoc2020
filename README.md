81add2a0113fdb8e7a555ee268e2d9a4792a7c0f
Takaloy <4484516+Takaloy@users.noreply.github.com>
2020-12-09 17:28:41 +0000

    Day 7 part 1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Advent of Code 2020
 
 Some fun coding exercises whilst learning Scala.
+https://adventofcode.com/2020/
---
+++ src/day7/Day7.scala
@@ -0,0 +1,55 @@
+package day7
+
+import scala.io.Source
+import scala.annotation.tailrec
+
+object Day7 extends App {
+
+  val fileName = "src/day7/input.txt"
+  val lines = Source.fromFile(fileName).getLines().toList
+
+  val records = lines.map(l => {
+    val r = l.split("contain")
+    (removeExcess(r(0)),
+      removeExcess(r.drop(1).mkString(""))
+        .split(",")
+        .filter(_ != "no other")
+        .map(a => {
+          val b = a.trim.split(" ")
+          (b.drop(1).mkString(" "), b(0).toInt)
+        }).toSet
+    )}).toMap
+
+  def removeExcess(input: String) : String = {
+    val regex = "(\\bbag[s]{0,1}[.]{0,1})".r
+    regex.replaceAllIn(input,"").trim
+  }
+
+  //test records are populated correctly
+  //records.foreach(r => println(s"${r._1} | ${r._2.mkString(" ")}"))
+
+  def canFitIntoBags(policy: Map[String, Set[(String, Int)]], target: String, subject: String) : Boolean = {
+
+    @tailrec
+    def canFitIn(target: String, consideredBag: Set[(String, Int)], discoveredBag: Set[(String, Int)]) : Boolean = {
+      if (discoveredBag.isEmpty)
+        false
+      else {
+        val bag = discoveredBag.head
+        if (bag._1 == target)
+          true
+        else if (consideredBag.exists(c => c._1 == bag._1))
+          canFitIn(target,consideredBag,discoveredBag.tail)
+        else
+          canFitIn(target, consideredBag + bag, discoveredBag.tail ++ policy(bag._1))
+      }
+    }
+    canFitIn(target,Set(), policy(subject))
+  }
+
+  def haveNumberOfOptionsFor(policy: Map[String, Set[(String, Int)]], target: String) : Int = {
+    policy.count(p => canFitIntoBags(policy, target, p._1))
+  }
+
+  println(haveNumberOfOptionsFor(records,"shiny gold"))
+}

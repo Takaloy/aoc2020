79734051cd5c4d1411a6759e1f6862d48205b884
Takaloy <4484516+Takaloy@users.noreply.github.com>
2020-12-09 00:24:37 +0000

    day 4 part 2

--- src/day4/Day4.scala
+++ src/day4/Day4.scala
@@ -24,9 +24,27 @@ object Day4 extends App {
 
     val requiredFields = List("byr","iyr","eyr","hgt","hcl","ecl","pid")
 
-    requiredFields.forall(r => kv.contains(r))
+    requiredFields.forall(r => kv.contains(r)) && kv.forall(k => isValidData(k._1,k._2))
+  }
+
+  def isValidData(k : String, v : String) : Boolean = (k,v) match {
+    case ("byr", v) => v.length == 4 && v.toInt >= 1920 && v.toInt <= 2002
+    case ("iyr", v) => v.length == 4 && v.toInt >= 2010 && v.toInt <= 2020
+    case ("eyr", v) => v.length == 4 && v.toInt >= 2020 && v.toInt <= 2030
+    case ("hgt", v) => hgtMatch(v)
+    case ("hcl", v) => v matches "^#[a-fA-F0-9]{6}$"
+    case ("ecl", v) => List("amb","blu","brn","gry","grn","hzl","oth").contains(v)
+    case ("pid", v) => v matches "^[0-9]{9}$"
+    case _ => true
+  }
+
+  def hgtMatch(v: String) : Boolean = v match {
+    case v if v.endsWith("cm") => v.substring(0,v.length-2).toInt >= 150 && v.substring(0,v.length-2).toInt <=193
+    case v if v.endsWith("in") => v.substring(0,v.length-2).toInt >= 59 && v.substring(0,v.length-2).toInt <=76
+    case _ => false
   }
 
   val validPassports = test count isValid
   println(f"valid passports $validPassports of ${test.length}")
+
 }

7c421ee5c9c50651a3f8102bf29d22d57be1e786
Takaloy <4484516+Takaloy@users.noreply.github.com>
2020-12-15 02:09:58 +0000

    Day 10 Part 2. This was HARD

--- src/day10/Day10.scala
+++ src/day10/Day10.scala
@@ -7,6 +7,7 @@ object Day10 extends App {
   val fileName = "src/day10/input.txt"
   val lines = Source.fromFile(fileName).getLines().map(_.toInt).toVector.sorted
   val jolts = 0 +: lines :+ lines.last + 3
+  println(jolts)
 
   def getJoltDifferences(jolts: Vector[Int]) : Set[(Int,Int)] = {
     @tailrec
@@ -26,4 +27,29 @@ object Day10 extends App {
   println(setGrouped)
   val answer = setGrouped.find(l => l._1 == 1).head._2 * setGrouped.find(l => l._1 == 3).head._2
   println(s"Day 10 Part 1 answer is : $answer" )
+
+  def calculatePossibleRoutes(jolts: Vector[Int]): Vector[BigInt] = {
+
+    @tailrec
+    def routeCalcSubRoutine(jolts: Vector[Int], counter : Int, accumulator: Vector[BigInt]) : Vector[BigInt] = {
+      if (counter == jolts.length-1)
+        accumulator
+      else if (counter == 0)
+        routeCalcSubRoutine(jolts, counter + 1, accumulator :+ 1)
+      else {
+        val acceptJolt = Vector(1,2,3)
+        val paths = acceptJolt.map(jolts(counter) - _)
+          .filter(x => x >= 0 && jolts.exists(j => j == x))
+          .map(b => accumulator(jolts.indexOf(b)))
+          .sum
+
+        println(s"sum of all paths for $counter is $paths")
+        routeCalcSubRoutine(jolts, counter + 1, accumulator :+ paths)
+      }
+    }
+
+    routeCalcSubRoutine(jolts, 0, Vector())
+  }
+
+  println(s"Day 10 part 2 answer is ${calculatePossibleRoutes(jolts).last}")
 }

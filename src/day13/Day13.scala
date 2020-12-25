f2d74f4da021ab5f738d0c8b6f3b64fc398f17eb
Takaloy <4484516+Takaloy@users.noreply.github.com>
2020-12-25 23:12:37 +0000

    copy formula from internet mwahaha

--- src/day13/Day13.scala
+++ src/day13/Day13.scala
@@ -31,63 +31,6 @@ object Day13 extends App {
 
   println(positionAndIds)
 
-//  def getStep1(positionAndIds : Vector[(Int,Int)], index: Int = 0, accumulator: String = "") : String = {
-//    if (positionAndIds.length == index)
-//      accumulator
-//    else {
-//      val step1 = positionAndIds
-//        .filter(_ != positionAndIds(index))
-//        .map(a => BigInt(a._2))
-//        .reduce(_ * _)
-//
-//      val mod = positionAndIds(index)._2
-//      val desiredRemainder = positionAndIds(index)._1
-//
-//      //val step2 : (BigInt, Int, Int) = (step1,(step1 % mod).toInt, bruteForceFindRightMultiplier((step1 % mod).toInt,desiredRemainder,mod))
-//
-//      val step2 = (step1, (step1 % mod))
-//      getStep1(positionAndIds, index+1, accumulator + " + " + step2)
-//    }
-//  }
-//
-//  def getPart2Result(positionAndIds : Vector[(Int,Int)], index: Int = 0, accumulator: BigInt = 0) : BigInt = {
-//    if (positionAndIds.length == index)
-//      accumulator
-//    else {
-//      val step1 = positionAndIds
-//        .filter(_ != positionAndIds(index))
-//        .map(a => BigInt(a._2))
-//        .reduce(_ * _)
-//
-//      val mod = positionAndIds(index)._2
-//      val desiredRemainder = (mod - positionAndIds(index)._1) % mod
-//
-//      val step2 = step1 * bruteForceFindRightMultiplier((step1 % mod).toInt, desiredRemainder, mod)
-//
-//      getPart2Result(positionAndIds, index+1, accumulator + step2)
-//    }
-//  }
-//
-//  @tailrec
-//  def bruteForceFindRightMultiplier(currentRemainder: Int, desiredRemainder: Int, modulo: Int, index: Int = 1): Int = {
-//    if ((currentRemainder * index) % modulo == desiredRemainder)
-//      index
-//    else if (desiredRemainder > modulo) {
-//      //bruteForceFindRightMultiplier(currentRemainder, desiredRemainder, modulo * (desiredRemainder/modulo + 1), index)
-//      bruteForceFindRightMultiplier(currentRemainder, desiredRemainder % modulo, modulo, index)
-//    }
-//    else {
-//      bruteForceFindRightMultiplier(currentRemainder,desiredRemainder,modulo,index+1)
-//    }
-//  }
-//
-//  //println(getStep1(positionAndIds))
-//  //println(getPart2Result(positionAndIds))
-//
-//  //chineseRemainder(List(41,37,379,23,13,17,29,557,19),List(0,35,41,49,54,58,70,72,91))
-//  //val result = chineseRemainder(List(41,37,379,23,13,17,29,557,19),List(0,35,41,49,54,58,70,72,91))
-
-
   val remainders = chineseRemainder(positionAndIds.map(a => BigInt(a._2)).toList,
     positionAndIds.map(a => BigInt(a._2 - (if (a._1 != 0) a._1 % a._2 else a._2))).toList)
   println(remainders)
